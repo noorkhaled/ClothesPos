30b9a1cf3ecc22e108cf6d567c682d756ef7f44c
noorkhaled <49693643+noorkhaled@users.noreply.github.com>
2023-07-03 13:16:12 +0300

    Update consts.js

--- src/store/consts.js
+++ src/store/consts.js
@@ -1,4 +1,4 @@
 
 
- export const serverBasePath ="http://localhost:61380/";
+ export const serverBasePath ="http://amirnono1-001-site1.ctempurl.com/";
 //export const serverBasePath ="http://192.200.1.89:96/";
